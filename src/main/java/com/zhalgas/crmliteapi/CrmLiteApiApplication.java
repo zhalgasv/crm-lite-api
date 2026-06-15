9f733e025676e8da4c25d912a4b71f3b6b135dd1
zhalgasadil <zhalgasvactor@gmail.com>
2026-06-15 16:10:16 +0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/zhalgas/crmliteapi/CrmLiteApiApplication.java
@@ -0,0 +1,13 @@
+package com.zhalgas.crmliteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmLiteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmLiteApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhalgas/crmliteapi/CrmLiteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhalgas.crmliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmLiteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
